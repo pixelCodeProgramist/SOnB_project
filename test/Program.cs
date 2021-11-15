aa4851e2e326844b8be807ce8b6ceb746f7e9947
Karol <karol9727@o2.pl>
2021-11-15 22:32:40 +0100

    test added

---
+++ test/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static string message = "11001101";
+        static string divider = "100101";
+
+        static void Main(string[] args)
+        {
+            message += "00000";
+            computeCRC();
+        }
+
+        static string xor(string message, string divider,ref int pos, string output)
+        {
+            string tempOutput = "";
+            for(int i = 0; i < divider.Length; i++)
+            {
+                if (divider[i] == output[i]) tempOutput += "0";
+                else tempOutput += "1";
+            }
+            
+            int zerosNumber = countZeroPrefix(tempOutput);
+            tempOutput = tempOutput.Substring(zerosNumber, tempOutput.Length-zerosNumber);
+            if(pos!=message.Length) tempOutput += message.Substring(pos, zerosNumber);
+            pos += zerosNumber;
+            Console.WriteLine(tempOutput);
+            return tempOutput;
+        }
+
+        static int countZeroPrefix(string str)
+        {
+            int count = 0;
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (str[i]=='0') 
+                    count++;
+                else break;
+            }
+            return count;
+        }
+
+        static void computeCRC()
+        {
+            int pos = divider.Length;
+            string helpMessage = message.Substring(0, divider.Length);
+            while (pos <= message.Length)
+               helpMessage = xor(message, divider, ref pos, helpMessage);
+
+        }
+    }
+}
